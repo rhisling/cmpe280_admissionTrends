c3cf766e414561b3a2d5fe1860d81a9377982e4b
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-08 12:54:49 -0800

    Server changes

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  console.log('hello');
-  res.send('hello');
+  res.send('Welcome to admission Trends');
 });
 
 app.listen(port, () => {
