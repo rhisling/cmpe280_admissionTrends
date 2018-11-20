aae4a42a168a31eaf8351a90b9325af17931f4eb
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-19 16:30:35 -0800

    Adding title dynamically

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', 'views');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.render('sign-in.ejs');
+  res.render('sign-in.ejs', { title: 'Admission Trends' });
 });
 
 app.listen(port, () => {
