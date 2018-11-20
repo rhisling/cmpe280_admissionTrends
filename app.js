5cdf33312bccb697ae59899858b10327c18de2ec
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-19 16:10:56 -0800

    Removing the client folder and setting up the login page

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 app.set('view-engine', 'ejs');
+app.use(express.static('public'));
 app.set('views', 'views');
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', { date: new Date() });
+  res.render('sign-in.ejs');
 });
 
 app.listen(port, () => {
