2cd5319a3de9ecf48dec21127e083658d95ea530
Rucha Apte <rucha.apte@sjsu.edu>
2018-11-21 13:36:12 -0800

    Merge branch 'master' of https://github.com/rhisling/cmpe280_admissionTrends
    change render to redirect index

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -39,7 +39,7 @@ router.post('/login', function(req, res) {
     var token = jwt.sign({ id: user._id }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
-      res.render('index.ejs')
+      res.redirect("/index");
     //res.status(200).send({ auth: true, token: token });
   });
 });
