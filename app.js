38dc291e79cfeb82b98e848ac1c8b1fc7c1849af
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-22 00:39:31 -0800

    Cleaning app.js and organizing it

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 const key = require('./config/mongodb-key');
-//const routes = require('./routes/routes');
+
+const routes = require('./routes/routes');
 const adminRoutes = require('./routes/admin');
 const dashboardRoutes = require('./routes/dashboard');
 
-
-
 const app = express();
+const store = new MongoDBStore({
+  uri: key.mongodbUrl,
+  collection: 'sessions'
+});
 
-app.set('view-engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'bigcatfish',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use((req, res, next) => {
+  res.set(
+    'Cache-Control',
+    'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+  );
+  next();
+});
 
 const port = process.env.PORT || 3000;
 
-app.get('/api', function (req, res) {
-  res.status(200).send('API works.');
-});
-
 /* JWT authentication*/
-
-const AuthController = require('./auth/AuthController');
-app.use('/api/auth',AuthController);
-
-
-app.get('/', (req, res) => {
-  res.render('sign-in.ejs', { title: 'Admission Trends' });
-});
-
-app.get('/sign-up', (req, res) => {
-  res.render('sign-up.ejs', { title: 'Admission Trends' });
-});
-
-
 
 app.get('/admin', (req, res) => {
   res.render('admin.ejs'); //?
@@ -54,14 +57,8 @@ app.get('/admin/delete-entry', (req, res) => {
 app.get('/admin/find-entry', (req, res) => {
   res.render('find-entry.ejs');
 });
-//app.use('/', routes);
-app.use(adminRoutes);
-app.use(dashboardRoutes);
 
-
-
-
-//app.use('/', routes);
+app.use('/', routes);
 app.use(adminRoutes);
 app.use(dashboardRoutes);
 
@@ -72,6 +69,6 @@ mongoose
   )
   .then(result => {
     console.log('MongoDB client connected');
-    app.listen(port, () => console.log('server is up on port',port));
+    app.listen(port, () => console.log('server is up on port', port));
   })
   .catch(err => console.log(err));
