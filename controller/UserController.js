8ca52d458d1a74828fdd62fc75ee9c429c47d298
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-26 11:37:22 -0800

    Fixing administrator showing in User page

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,21 +1,19 @@
 const Ustats = require('../models/ustats');
 
-
 module.exports.getScoreStats = (req, res) => {
-    //console.log(req.body);
-    //Ustats.find(req.body)
-    //console.log(req.query);
-    var dict={}
+  //console.log(req.body);
+  //Ustats.find(req.body)
+  //console.log(req.query);
+  var dict = {};
 
-    var obj = JSON.parse('{ "YEAR":"2017"}');
-    //console.log(obj)
-   // console.log(typeof obj)
-    //console.log(typeof obj.year)
-    Ustats.find(obj,{"INSTNM":1,"SAT_AVG":1,"GPA_Val":1})
-        .then(results =>{
-
-            res.send(JSON.stringify(results));
-            /*
+  var obj = JSON.parse('{ "YEAR":"2017"}');
+  //console.log(obj)
+  // console.log(typeof obj)
+  //console.log(typeof obj.year)
+  Ustats.find(obj, { INSTNM: 1, SAT_AVG: 1, GPA_Val: 1 })
+    .then(results => {
+      res.send(JSON.stringify(results));
+      /*
             console.log("results for score stats",results);
             //results[i]['GPA']
             //console.log("results for score stats",results[0]['YEAR'])
@@ -39,28 +37,22 @@ module.exports.getScoreStats = (req, res) => {
             }
           //console.log("list is ",list[0],list[1])
 */
-        } )
-        .catch(err => res.send(err));
-
-
-
+    })
+    .catch(err => res.send(err));
 };
-
 
 module.exports.showStats = (req, res) => {
-
-
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('fetchScoreStats.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
-}
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('fetchScoreStats.ejs', {
+    isAuthenticated: req.user.name === 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
+};
 
 function capitalize(s) {
-    return s && s[0].toUpperCase() + s.slice(1);
-};
+  return s && s[0].toUpperCase() + s.slice(1);
+}
