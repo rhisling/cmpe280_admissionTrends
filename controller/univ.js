a407e403866c755828dc37c401fa06a45b550abb
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-26 11:10:48 -0800

    Adding admin authentication

--- controller/univ.js
+++ controller/univ.js
@@ -1,107 +1,105 @@
-
 module.exports.showUcb = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucb.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucb.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUci = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('uci.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('uci.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 
 module.exports.showUcsd = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucsd.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucsd.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 
 module.exports.showUcla = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucla.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucla.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUcsb = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucsb.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucsb.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUcr = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucr.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucr.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUcm = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucm.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucm.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUcsc = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucsc.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucsc.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
 module.exports.showUcd = (req, res) => {
-    const user = req.session.user || req.user;
-    const photo = req.user.photo || false;
-    res.render('ucd.ejs', {
-        isAuthenticated: true,
-        user: capitalize(user.name).split(' ')[0],
-        message: false,
-        title: 'Admission Trends',
-        photo: photo
-    });
+  const user = req.session.user || req.user;
+  const photo = req.user.photo || false;
+  res.render('ucd.ejs', {
+    isAuthenticated: req.user.name == 'admin',
+    user: capitalize(user.name).split(' ')[0],
+    message: false,
+    title: 'Admission Trends',
+    photo: photo
+  });
 };
-
 
 function capitalize(s) {
-    return s && s[0].toUpperCase() + s.slice(1);
-};
+  return s && s[0].toUpperCase() + s.slice(1);
+}
