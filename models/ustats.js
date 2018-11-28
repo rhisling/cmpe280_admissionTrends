4b6b4eb9aa28903c10a73a07ec057e260fa0df83
Rucha Apte <rucha.apte@sjsu.edu>
2018-11-27 18:25:34 -0800

    added new cols to schema

--- models/ustats.js
+++ models/ustats.js
@@ -28,11 +28,56 @@ const ucstatsSchema = new Schema({
     default:'0'
     //required: true
   },
-    GPA_Val: {
-        type: String,
-        default:'0'
-        //required: true
-    }
+  GPA_Val: {
+      type: String,
+      default:'0'
+      //required: true
+  },
+  TUITIONFEE_OUT: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  RET_FT4: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  PCTFLOAN: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  PC_PARTTIME: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  IN_EXPENDITURE: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  PCTPELL: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  COMPLETION_RATE: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  UGDS_MEN: {
+    type: String,
+    default:'0'
+    //required: true
+  },
+  UGDS_WOMEN: {
+    type: String,
+    default:'0'
+    //required: true
+  }
 });
 
 module.exports = mongoose.model('Ustats', ucstatsSchema);
